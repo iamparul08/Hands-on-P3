54b511043066a1fc2d57f4f65a4037a0511fd767
Parul Mathur <46438911+iamparul08@users.noreply.github.com>
2020-05-18 13:40:42 +0530

    casting

---
+++ readkey2_ADID.py
@@ -0,0 +1,10 @@
+num1_ADID = input("Enter 100 ")
+num2_ADID = input("Enter 200 ")
+
+sum1_ADID = float(num1_ADID) + float(num2_ADID)
+print(sum1_ADID)
+
+sum1_ADID = int(num1_ADID) + int(num2_ADID)
+print(sum1_ADID)
+
+#Justification:- Both in float and int we got the result in float value i.e. 300.0
